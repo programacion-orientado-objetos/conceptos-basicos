ac28ef01f63d614120255458a2aa5c58f286e4e6
Javi1499 <fruiz1@ucol.mx>
2019-02-05 19:36:01 -0600

    actualizacion

---
+++ conceptos-basicos/auto.js
@@ -0,0 +1,10 @@
+class auto {
+constructor(marca, modelo, color, kilometraje) {
+this.marca = marca;
+this.modelo = modelo;
+this.colorm = color;
+this.kilometraje = kilometraje;
+this.estado = "Apagado";
+}
+
+}
